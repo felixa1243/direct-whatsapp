dea6cadc2a2d33b9d6c2c3f2eb689e300c6f1874
Felixarajiph <felixarajiph@gmail.com>
2022-08-21 15:40:25 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import Input from './components/Input'
 import Message from "./components/Message"
 import 'bulma/css/bulma.min.css'
+import {useEffect} from 'react'
+
 function App() {
+  useEffect(()=>document.title="direct whatsapp",[document.title])
   return (
     <div className="container px-2">
       <h1 className='is-size-5 mb-2'>Send message without ever saving a new number...</h1>
